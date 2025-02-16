cbe6bd660c5dc67511f37bedc9ab0cb825d0ea32
hu <3108967414@qq.com>
2025-02-16 16:31:14 +0800

    '来单提醒和检查订单状态功能开发'

---
+++ sky-server/src/main/java/com/sky/controller/user/OrderUserController.java
@@ -0,0 +1,17 @@
+package com.sky.controller.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 胡志坚
+ * @version 1.0
+ * 创造日期 2025/2/14
+ * 说明:
+ */
+@RestController
+@RequestMapping("/user/order")
+public class OrderController {
+
+
+}
